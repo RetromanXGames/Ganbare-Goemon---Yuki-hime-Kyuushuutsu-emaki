3c5d5cb0f35ca25f5139c20747e9aca2e95dc7c0
RetromanXGames <velkien@hotmail.com>
2021-01-13 16:18:28 -0300

    delected rotina

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 bass.exe patch.asm
 G:\RetroArch\retroarch.exe -L "G:\RetroArch\cores\bsnes2014_accuracy_libretro.dll" "G:\Projetos\Ganbare Goemon - Yuki hime Kyuushuutsu emaki\Ganbare Goemon - Yuki hime Kyuushuutsu emaki (BR).sfc"
+G:\RetroArch\retroarch.exe -L "G:\RetroArch\cores\bsnes2014_accuracy_libretro.dll" "G:\Projetos\Ganbare Goemon - Yuki hime Kyuushuutsu emaki\Ganbare_Goemon(BR).sfc"
--- preview.bat
+++ preview.bat
@@ -1,1 +1,1 @@
-G:\RetroArch\retroarch.exe -L "G:\RetroArch\cores\bsnes2014_accuracy_libretro.dll" "G:\Projetos\Ganbare Goemon - Yuki hime Kyuushuutsu emaki\Ganbare Goemon - Yuki hime Kyuushuutsu emaki (BR).sfc"
+G:\RetroArch\retroarch.exe -L "G:\RetroArch\cores\bsnes2014_accuracy_libretro.dll" "G:\Projetos\Ganbare Goemon - Yuki hime Kyuushuutsu emaki\Ganbare_Goemon(BR).sfc"
